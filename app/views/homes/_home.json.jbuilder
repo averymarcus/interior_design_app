42736e353f50dd62210554d1ca1f4c7c31ec682d
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-03-10 16:46:56 +0000

    Generate homes resource with ideas:scaffold

---
+++ app/views/homes/_home.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! home, :id, :user_id, :home_address, :home_image, :created_at, :updated_at
+json.url home_url(home, format: :json)
---
+++ db/migrate/20220310164655_create_homes.rb
@@ -0,0 +1,11 @@
+class CreateHomes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :homes do |t|
+      t.integer :user_id
+      t.string :home_address
+      t.string :home_image
+
+      t.timestamps
+    end
+  end
+end
